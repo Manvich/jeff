ef1c2a24fc40c8792e87ce4b74a436da0e2bf92c
Manvich <_jellyfish_@berkeley.edu>
2017-11-06 13:52:50 -0800

    updating circles

--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,16 @@
-var winW;
-var winH;
-var img;
-var circles;
+var winW, winH, img, circles, size, color;
 
 function preload() {
-	img = loadImage("cuts/untrimmed2.jpg");
+	img = loadImage("cuts/untrimmed3.jpg");
 }
 
 function setup() {
 	winW = window.innerWidth;
 	winH = window.innerHeight;
 	createCanvas(winW, winH);
-	background(240);
-	image(img, x, y);
 	circles = new Array();
+	size = 10;
+	color = 255;
 }
 
 function draw() {
@@ -24,14 +21,22 @@ function draw() {
 		winH = window.innerHeight;
 		createCanvas(winW, winH);
 	}
-	noStroke();
-	fill(255);
+	
+	background(240);
+	image(img, x, y);
+	
 	if (mouseIsPressed &&
 		mouseX > x && mouseX < x + 500 &&
-		mouseY > y && mouseY < y + 500) {
-		circles.push(ellipse(mouseX, mouseY, 10));
-		print(circles[circles.length - 1].args[0]);
+		mouseY > y && mouseY < y + 475) {
+		circles.push([mouseX - x, mouseY - y, size, color]);
 	}
+	
+	noStroke();
+	for (var i = 0; i < circles.length; i++) {
+		fill(circles[i][3]);
+		ellipse(circles[i][0], circles[i][1], circles[i][2]);
+	}
+	
 	stroke();
 	nofill();
 	rect(x, y, 500, 600);
