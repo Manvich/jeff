5ffa2939316d63ff17d4e1d601313548191dec26
Manvich <_jellyfish_@berkeley.edu>
2017-11-06 15:06:23 -0800

    only redraw if window changes

--- js/sketch.js
+++ js/sketch.js
@@ -14,28 +14,34 @@ function setup() {
 }
 
 function draw() {
+	var x = winW / 2 - 250;
+	var y = winH / 2 - 300;
+	
 	if (window.innerWidth != winW || window.innerHeight != winH) {
 		winW = window.innerWidth;
 		winH = window.innerHeight;
 		createCanvas(winW, winH);
+		
+		x = winW / 2 - 250;
+		y = winH / 2 - 300;
+		
+		background(240);
+		image(img, x, y);
+		
+		noStroke();
+		for (var i = 0; i < circles.length; i++) {
+			fill(circles[i][3]);
+			ellipse(x + circles[i][0], y + circles[i][1], circles[i][2]);
+		}
 	}
-	
-	var x = winW / 2 - 250;
-	var y = winH / 2 - 300;
-	
-	background(240);
-	image(img, x, y);
 	
 	if (mouseIsPressed &&
 		mouseX > x && mouseX < x + 500 &&
 		mouseY > y && mouseY < y + 450) {
 		circles.push([mouseX - x, mouseY - y, s, c]);
-	}
-	
-	noStroke();
-	for (var i = 0; i < circles.length; i++) {
-		fill(circles[i][3]);
-		ellipse(x + circles[i][0], y + circles[i][1], circles[i][2]);
+		noStroke();
+		fill(c);
+		ellipse(mouseX, mouseY, s);
 	}
 	
 	stroke(0);
