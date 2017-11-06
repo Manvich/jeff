c8dcc13e7cd2827ef333f77628d8b9b0f99d2b33
Manvich <_jellyfish_@berkeley.edu>
2017-11-06 15:33:57 -0800

    implement colour and size sliders

--- js/sketch.js
+++ js/sketch.js
@@ -1,34 +1,25 @@
-var winW, winH, img, circles, s, c;
+var winW, winH, img, circles;
+var cSlider, sSlider;
+var c, s;
 
 function preload() {
 	img = loadImage("cuts/untrimmed3.jpg");
 }
 
 function setup() {
-	strokeWeight(10);
-	winW = window.innerWidth;
-	winH = window.innerHeight;
-	createCanvas(winW, winH);
+	init();
 	circles = new Array();
-	s = 20;
-	c = 255;
 }
 
 function draw() {
 	var x = winW / 2 - 250;
 	var y = winH / 2 - 300;
 	
+	s = sSlider.value();
+	c = cSlider.value();
+	
 	if (window.innerWidth != winW || window.innerHeight != winH) {
-		winW = window.innerWidth;
-		winH = window.innerHeight;
-		createCanvas(winW, winH);
-		
-		x = winW / 2 - 250;
-		y = winH / 2 - 300;
-		
-		background(240);
-		image(img, x, y);
-		
+		init();
 		noStroke();
 		for (var i = 0; i < circles.length; i++) {
 			fill(circles[i][3]);
@@ -46,6 +37,29 @@ function draw() {
 	}
 	
 	stroke(0);
+	strokeWeight(20);
 	noFill();
 	rect(x, y, 500, 600);
 }
+
+function init() {
+	winW = window.innerWidth;
+	winH = window.innerHeight;
+	createCanvas(winW, winH);
+	
+	x = winW / 2 - 250;
+	y = winH / 2 - 300;
+	
+	background(240);
+	image(img, x, y);
+	
+	cSlider = createSlider(0, 1, 1);
+	cSlider.position(x + 10, y + 625);
+	sSlider = createSlider(5, 100, 20);
+	sSlider.position(x + 10, y + 650);
+}
+
+//implementations:
+//undo button
+//black and white pens
+//slider to change size
