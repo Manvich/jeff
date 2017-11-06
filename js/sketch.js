3ae38bd4fc4668c23adae2793bc4eaa1311fc097
Manvich <_jellyfish_@berkeley.edu>
2017-11-06 13:35:57 -0800

    bugtesting

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,7 @@
 var winW;
 var winH;
 var img;
+var circles;
 
 function preload() {
 	img = loadImage("cuts/untrimmed2.jpg");
@@ -10,20 +11,28 @@ function setup() {
 	winW = window.innerWidth;
 	winH = window.innerHeight;
 	createCanvas(winW, winH);
+	circles = new Array();
 }
 
 function draw() {
+	var x = winW / 2 - 250;
+	var y = winH / 2 - 300;
 	if (window.innerWidth != winW || window.innerHeight != winH) {
 		winW = window.innerWidth;
 		winH = window.innerHeight;
 		createCanvas(winW, winH);
 	}
 	background(240);
-	image(img, winW / 2 - 250, winH / 2 - 300);
+	image(img, x, y);
+	noStroke();
 	fill(255);
 	if (mouseIsPressed &&
-		mouseX > winW / 2 - 250 && mouseX < winW / 2 + 250 &&
-		mouseY > winH / 2 - 300 && mouseY < winH / 2 + 300) {
-		ellipse(mouseX, mouseY, 10, 10);
+		mouseX > x && mouseX < x + 500 &&
+		mouseY > y && mouseY < y + 500) {
+		circles.push(ellipse(mouseX, mouseY, 10));
+		print(circles[circles.length - 1].args[0]);
 	}
+	stroke();
+	nofill();
+	rect(x, y, 500, 600);
 }
